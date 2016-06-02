f2f03950d18e61d0bbff2d546295088464bfa24d
Jake Elder <jake@jpeg.me.uk>
2016-06-02 16:56:01 +0100

    Add socket to app

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 import express from 'express';
+import { Server } from 'http';
 
 import hi from './hi';
 import api from './api';
+import socket from './socket';
 import * as db from './database';
 
-const server = express();
+const app = express();
+const server = new Server(app);
 
-server.use('/', hi);
-server.use('/api', api);
+app.use('/', hi);
+app.use('/api', api);
+socket.attach(server);
 
 db.awaitConnection.then(
   () => server.listen(3000, error => {
